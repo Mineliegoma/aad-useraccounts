fab149fc04e396baa286df2540c0c48462bd56f6
Mineliegoma <62627823+Mineliegoma@users.noreply.github.com>
2023-07-12 01:09:18 -0400

    added SG and logs

--- script.ps1
+++ script.ps1
@@ -12,3 +12,23 @@ for ($i = 1; $i -le 20; $i++) {
  $mailNickname = "test.user$i"
  New-AzADUser -DisplayName $displayName -Password $password -UserPrincipalName $userPrincipalName -ForceChangePasswordNextLogin -MailNickname $mailNickname
 }
+# Create a new Azure AD group
+$group = New-AzADGroup -DisplayName "Varonis Assignment Group" -MailNickname "AssignmentGroup"
+
+# Add each user to the group and log the attempt
+$logFile = "UserGroupLog.txt"
+for ($i = 1; $i -le 20; $i++) {
+    $userPrincipalName = "test.user$i@mineliegomalembetoutlook.onmicrosoft.com"
+    $user = Get-AzADUser -UserPrincipalName $userPrincipalName
+    try {
+        Add-AzADGroupMember -TargetGroupObjectId $group.Id -MemberObjectId $user.Id
+        $result = "Success"
+    }
+    catch {
+        $result = "Failure: " + $_.Exception.Message
+    }
+    finally {
+        $logEntry = "Username: $userPrincipalName, Timestamp: $(Get-Date), Result: $result"
+        Add-Content -Path $logFile -Value $logEntry
+    }
+}
